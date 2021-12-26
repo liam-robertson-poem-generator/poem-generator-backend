54e714fd866d56834b29e3d762bc06b3397a97aa
Liam-Robertson <liamjohnrobertson@gmail.com>
2021-12-26 13:52:32 +0000

    fully working algo, printout working

--- src/main/scala/poemGenerator/ExportController.scala
+++ src/main/scala/poemGenerator/ExportController.scala
@@ -1,7 +1,35 @@
 package poemGenerator
 
+import com.spire.doc.{Document, FileFormat, Section}
+import poemGenerator.controllerUtilites.ExportUtilities
+
 class ExportController {
 
+  case class Poem(code: String, title: String, text: String, glyph: String)
   val inputController = new InputController();
+  val exportUtilities = new ExportUtilities();
 
+  def exportToWord() = {
+    val poemList = inputController.iterateSorted2dArray(4, Array(1,1,9), "forwards");
+    val document = new Document();
+
+    exportUtilities.settingDocStyles(document);
+
+    poemList.map(poem => {
+      val currentPoemCode = poem.mkString("-")
+      val poemPath = this.getClass.getResource("/poems/" + currentPoemCode + ".xml")
+      val glyphPath = this.getClass.getResource("/glyphs/" + currentPoemCode + ".jpg")
+
+      val section = document.addSection();
+      val para1 = section.addParagraph();
+      val para2 = section.addParagraph();
+      val para3 = section.addParagraph();
+
+      val (currentPoemTitle, currentPoemText) = exportUtilities.parsingXml(poemPath);
+      exportUtilities.creatingDocContent(currentPoemCode, currentPoemTitle, glyphPath, currentPoemText, para1, para2, para3)
+
+    })
+    document.saveToFile("Output.docx", FileFormat.Docx);
+    ""
+  }
 }
--- src/main/scala/poemGenerator/GeneratorService.scala
+++ src/main/scala/poemGenerator/GeneratorService.scala
@@ -6,24 +6,22 @@ import akka.http.scaladsl.server.Route
 class GeneratorService {
 
   val inputController = new InputController();
+  val exportController = new ExportController();
 
   val routes: Route =
     get {
       pathSingleSlash {
         complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, inputController.serverStatusMessage))
       } ~
-      pathPrefix("poems") {
-        getFromDirectory("src/main/resources/poems")
-      } ~
-      pathPrefix("glyphs") {
-        getFromDirectory("src/main/resources/glyphs")
-      } ~
       path("listAll") {
         complete(HttpEntity(ContentTypes.`application/json`, inputController.getSorted2dArray().toString))
       } ~
         path("getIteratedList") {
-          complete(HttpEntity(ContentTypes.`application/json`, inputController.iterateSorted2dArray(2271, Array(1,1,9), "forwards").toString))
-        }
+          complete(HttpEntity(ContentTypes.`application/json`, inputController.iterateSorted2dArray(20, Array(1,1,9), "forwards").toString))
+      } ~
+      path("saveDoc") {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, exportController.exportToWord()))
+      }
     }
 
 }
