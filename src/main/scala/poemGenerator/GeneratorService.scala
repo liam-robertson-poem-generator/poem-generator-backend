b9fb789980433d303bf0efb440f244cb4701405d
Liam Robertson <liamjohnrobertson@gmail.com>
2022-01-16 01:26:00 +0000

    fixed error handling

--- src/main/scala/poemGenerator/GeneratorService.scala
+++ src/main/scala/poemGenerator/GeneratorService.scala
@@ -1,61 +1,47 @@
 package poemGenerator
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, MediaTypes}
-import akka.http.scaladsl.server.Directives.{entity, _}
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse, MediaTypes}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server._
 import akka.util.ByteString
 import poemGenerator.controllers.{InputController, PrimaryController}
-import poemGenerator.tools.{CORSHandler, ExceptionHandling}
+import poemGenerator.tools.CORSHandler
 import spray.json.DefaultJsonProtocol
 
-class GeneratorService extends SprayJsonSupport with DefaultJsonProtocol with  CORSHandler with ExceptionHandling {
+class GeneratorService extends SprayJsonSupport with DefaultJsonProtocol with  CORSHandler {
   val inputController = new InputController();
   val primaryController = new PrimaryController();
 
   case class generatorParameters(startingPoem: Array[Int], numOfPoems: Int, poemOrder: String)
   implicit val parametersFormat = jsonFormat3(generatorParameters)
 
-  val routes: Route =
-    concat(
-      corsHandler(
-        get {
-          pathSingleSlash {
-            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, inputController.serverStatusMessage))
+  def myExceptionHandler: ExceptionHandler =
+    ExceptionHandler {
+      case cause: Exception =>
+        complete(HttpResponse(500, entity = cause.getMessage)
+        )
+    }
+
+        val routes: Route =
+      concat(
+        corsHandler(
+          handleExceptions(myExceptionHandler) {
+            get {
+              pathSingleSlash {
+                complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, inputController.serverStatusMessage))
+              }
+              path("listAllPoems") {
+                complete(inputController.getSorted2dArray())
+              } ~
+              path("createDocument") {
+                parameters("numOfPoems".as[Int], "startingPoem".as[Array[Int]], "poemOrder") { (numOfPoems: Int, startingPoem: Array[Int], poemOrder: String) =>
+                  val docByteArray: ByteString = primaryController.primaryExecutor(numOfPoems, startingPoem, poemOrder)
+                  val entity = HttpEntity.Strict(MediaTypes.`application/octet-stream`, docByteArray)
+                  complete(entity)
+                }
+              }
+            }
           }
-          path("listAllPoems") {
-            exceptionHandler(() => {
-              inputController.getSorted2dArray()
-            })
-          } ~
-          path("createDocument") {
-            parameters("numOfPoems".as[Int], "startingPoem".as[Array[Int]], "poemOrder") { (numOfPoems: Int, startingPoem: Array[Int], poemOrder: String) =>
-              exceptionHandler(() => {
-                val docByteArray: ByteString = primaryController.primaryExecutor(numOfPoems, startingPoem, poemOrder)
-                val entity = HttpEntity.Strict(MediaTypes.`application/octet-stream`, docByteArray)
-                entity
-              })
-
-
-
-
-
-
-//              complete({
-//                val result: ToResponseMarshallable = try {
-//                  val docByteArray: ByteString = primaryController.primaryExecutor(numOfPoems, startingPoem, poemOrder)
-//                  val entity = HttpEntity.Strict(MediaTypes.`application/octet-stream`, docByteArray)
-//                  entity
-//                } catch {
-//                  case e: Throwable => {
-//                    errorMessage(e)
-//                  }
-//                }
-//                result
-//              })
-          }
-        }
-      }
-    )
-  )
+        )
+      )
 }
